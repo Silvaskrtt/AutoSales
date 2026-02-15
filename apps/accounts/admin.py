2f556944bae2a9618e55778ce94c379e7ada5e9e
Marcia silva <murilodasilvadaschagas12@gmail.com>
2026-02-15 15:22:30 -0300

    config: configurando painel do Banco de Dados referente a 'accounts'.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'username', 'is_staff', 'is_active', 'data_criacao')
+    list_filter = ('is_staff', 'is_active', 'status')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Informações Pessoais', {'fields': ('username', 'status')}),
+        ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Datas', {'fields': ('data_criacao',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+    readonly_fields = ('data_criacao',)
+
+admin.site.register(User, CustomUserAdmin)
