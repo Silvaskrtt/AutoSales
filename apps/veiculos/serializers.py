185f42d7b909bba831e682bbae4550c951280bf0
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-02-25 13:40:20 -0300

    ?? apps/veiculos/serializers.py

---
+++ apps/veiculos/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Veiculo
+
+class VeiculoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Veiculo
+        fields = [
+            'id', 'marca', 'modelo', 'ano', 'cor', 'preco',
+            'is_active', 'user'
+        ]
+        read_only_fields = ['id', 'user']
