dcfebc23025d7f7caf3f633e37b20c8c190e3fc5
Murilo Chagas <murilodasilvadaschagas12@gmail.com>
2026-02-25 10:14:46 -0300

    Atualiza apps/clientes/urls.py

--- apps/clientes/urls.py
+++ apps/clientes/urls.py
@@ -8,9 +8,6 @@ router.register(r'api/clientes', ClienteViewSet, basename='api-clientes')
 
 urlpatterns = [
     path('clientes/', GerenciarCliente.as_view(), name='lista_clientes'),
-    path('clientes/criar/', CriarCliente.as_view(), name='criar_cliente'),
-    path('clientes/editar/<int:pk>/', EditarCliente.as_view(), name='editar_cliente'),
-    path('clientes/desativar/<int:pk>/', DesativarCliente.as_view(), name='desativar_cliente'),
     # Rotas da API (DRF)
     path('', include(router.urls)),
 ]
